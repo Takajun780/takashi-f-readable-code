5555eb9daddc728fa5f7b21ac143e8d6b1d5dd0d
takashi-f <t.furuno@betasoft.co.jp>
2015-06-24 11:42:24 +0900

    spec3: レシピ情報をファイルから読み込むように変更しました。
    recipe-data.txtにレシピ情報を記述します。

--- readable-code/src/Recipe.java
+++ readable-code/src/Recipe.java
@@ -1,10 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author takashi-f
  */
 public class Recipe {
 
 	public static void main(String[] args) {
-		System.out.println("ƒIƒ€ƒ‰ƒCƒX");
+		try{
+			File recipeFile = new File("src/recipe-data.txt");
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(recipeFile));
+
+			String row;
+			while((row = bufferedReader.readLine()) != null){
+				System.out.println(row);
+			}
+			bufferedReader.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println(e);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
 	}
 
 }
