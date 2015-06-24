a973ab043a6e907f55e90cd21b7be5d68d9bfaea
Takajun780 <Takajun780@gmail.com>
2015-06-24 14:22:42 +0900

    spec6: "IDが指定された場合はそのレシピだけ出力できること"のコーディング完了。

--- readable-code/src/Recipe.java
+++ readable-code/src/Recipe.java
@@ -11,10 +11,11 @@ import java.util.List;
  */
 public class Recipe {
 
+	// spec6: IDはmainメソッドの引数で指定する。
 	public static void main(String[] args) {
 		List<String> recipeList = new ArrayList<String>();
 		try {
-			File recipeFile = new File("src/recipe-data.txt");
+			File recipeFile = new File("readable-code/src/recipe-data.txt");
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(recipeFile));
 
 			String row;
@@ -29,11 +30,34 @@ public class Recipe {
 		catch (IOException e) {
 			System.out.println("ファイルが読み込めませんでした。");
 		}
-		
-		for (int loop = 0; loop < recipeList.size(); loop++) {
-			System.out.println(loop + ": " + recipeList.get(loop));
+
+		// spec6: argsの要素数で場合分け。
+		if (args.length == 0)
+		{
+			// spec6: IDを指定されなかった場合の挙動はspec5と同じ。
+			for (int loop = 0; loop < recipeList.size(); loop++) {
+				System.out.println(loop + ": " + recipeList.get(loop));
+			}
 		}
-		
+		else if (args.length == 1)
+		{
+			// spec6: IDが指定された場合はそのレシピだけ出力できること。
+			int reservedId = Integer.parseInt(args[0]);
+			for (int loop = 0; loop < recipeList.size(); loop++) {
+				if (loop == reservedId){
+					System.out.println(loop + ": " + recipeList.get(loop));
+				}
+			}
+		}
+		else
+		{
+			System.out.println("IDの指定が正しくありません。");
+		}
+
+
+
 	}
+
+
 
 }
